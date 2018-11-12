6e38014b067bdf4ab048400b2ddc998c9d808836
dkc1011 <43175543+dkc1011@users.noreply.github.com>
2018-11-12 09:44:17 +0000

    Changelog 12/11/2018 09:44
    + Added do-whiles so that multiple option selections can be repeated upon error
    + Added the selected difficulty now affects the amount of money the player's crew will start the game with
    + Added the player's crew, created in Crew.java
    + Added empty method header startGame( )

--- src/GameManager.java
+++ src/GameManager.java
@@ -3,33 +3,65 @@
 import javax.swing.*;
 
 public class GameManager {
-    public static char difficulty;
+    public static char difficulty = 'd';
+    public Crew playerCrew = new Crew();
 
     public static void main(String[] args) {
         char choice = JOptionPane.showInputDialog("~~~High Seas Privateer~~~\n1.Start\n2.Exit").charAt(0);
-
-        switch (choice)
+        
+        do
         {
-            case '1':
-                selectDifficulty();
-
-            case '2':
-                System.exit(0);
-
-            default :
-                JOptionPane.showMessageDialog(null,"Please enter another option!","High Seas Privateer", JOptionPane.ERROR_MESSAGE);
-        }
+            switch (choice)
+            {
+                case '1':
+                    selectDifficulty();
+                    
+                default :
+                    JOptionPane.showMessageDialog(null,"Please enter another option!","High Seas Privateer", JOptionPane.ERROR_MESSAGE);
+            }
+        }while(choice != '2');
+        
+        System.exit(0);
 
     }
 
     private static void selectDifficulty()
     {
-        difficulty = JOptionPane.showInputDialog("Please select a difficulty!\nThe difficulty effects how much money for supplies you have in the begginning of the game." +
-                "\n\n1.Easy\n2.Moderate\n3.Hard").charAt(0);
-
-        if(difficulty != '1' && difficulty != '2' && difficulty != '3')
+        do
         {
-            JOptionPane.showMessageDialog(null,"Please enter a valid option.", "Error", JOptionPane.ERROR_MESSAGE);
-        }
+            difficulty = JOptionPane.showInputDialog("Please select a difficulty!\nThe difficulty effects how much money for supplies you have in the begginning of the game." +
+                 "\n\n1.Easy\n2.Moderate\n3.Hard").charAt(0);
+
+            if(difficulty != '1' && difficulty != '2' && difficulty != '3')
+            {
+                JOptionPane.showMessageDialog(null,"Please enter a valid option.", "Error", JOptionPane.ERROR_MESSAGE);
+                difficulty = 'd';
+            }
+            else
+            {
+                playerCrew.createCrew();
+            
+                if(difficulty == '1')
+                {
+                    playerCrew.setMoney(400);
+                }
+                else if(difficulty == '2')
+                {
+                    playerCrew.setMoney(300);
+                }
+                else
+                {
+                    playerCrew.setMoney(200);
+                }
+            
+                startGame();
+            }
+        }while(difficulty == 'd');
     }
+    
+    private static void startGame()
+    {
+        
+    }
+    
 }
